22c89ca2fd9d0c078593454dda3f63ccef142309
AfzalKamboh04 <afzalkam4@gmail.com>
2024-10-05 18:43:32 +0500

    All credentials related issue are resolved and log manging is added

--- app/db/database.py
+++ app/db/database.py
@@ -1,16 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.core.config import Settings
+import logging
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root123@localhost/parking_slot"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(Settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
     try:
         yield db
+    except Exception as e:
+        db.rollback()  
+        logging.error(f"Database session error: {e}")
+        raise
     finally:
-        db.close()
+        db.close()  # Ensure session is closed after use
